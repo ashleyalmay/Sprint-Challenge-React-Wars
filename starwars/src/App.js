af22328d232f911f48d0576cab42a0b3e6f47218
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-04-17 15:07:53 -0400

    done added 2 more cards

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Characters from "./components/Character";
-
-
+import Characters1 from "./components/Character2";
+import Characters2 from "./components/Character3";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -14,9 +14,10 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Information</h1>
+      <h1 className="Header">Pokemon Information</h1>
       <Characters/>
-     
+    <Characters1/>
+    <Characters2/>
     </div>
   );
 }
--- starwars/src/components/Moves.js
+++ starwars/src/components/Moves.js
@@ -1,16 +1,15 @@
 //moves
 // Write your Character component here
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import "../App";
-import  styled from 'styled-components';
+
 
 const Moves = props => {
 
-    console.log(props.move.name)
 return(
     <li>
-        {props.move.name}
+        {props.move.move.name}
     </li>
 )
 
